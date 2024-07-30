e33fcc4b6deb649b0a65e807c0137cc82fbc57d3
Mostafa Hammam <76970262+Mostafa-hemma@users.noreply.github.com>
2024-07-30 16:20:29 +0300

    edit somthing

--- demo/src/main/java/com/backend/demo/controller/AuthenticationController.java
+++ demo/src/main/java/com/backend/demo/controller/AuthenticationController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -34,6 +35,7 @@ import com.backend.demo.service.UserService;
 
 
 @RestController
+@CrossOrigin
 public class AuthenticationController {
 	
 	@Autowired
