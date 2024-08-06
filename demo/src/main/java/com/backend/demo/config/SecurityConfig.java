b2723cbb86eb485cfff7d4b65ab5c2bdc47aa3c3
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2024-08-07 01:43:28 +0300

    editing in permisions

--- demo/src/main/java/com/backend/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/backend/demo/config/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE, "/pickup/**").hasRole("CUSTOMER")
                 
                 .antMatchers(HttpMethod.GET, "/agent").hasAnyRole("ADMIN","MANAGER")
-                .antMatchers(HttpMethod.GET, "/agent/**").hasAnyRole("AGENT","ADMIN","MANAGER")
+                .antMatchers(HttpMethod.GET, "/agent/**").hasAnyRole("CUSTOMER","ADMIN","MANAGER")
                 .antMatchers(HttpMethod.POST, "/agent/**").hasAnyRole("ADMIN","MANAGER")
                 .antMatchers(HttpMethod.PUT, "/agent/**").hasAnyRole("ADMIN","MANAGER")
                 .antMatchers(HttpMethod.DELETE, "/agent/**").hasAnyRole("ADMIN","MANAGER")
